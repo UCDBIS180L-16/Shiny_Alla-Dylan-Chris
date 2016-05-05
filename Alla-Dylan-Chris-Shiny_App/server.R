a4914d31dc6786f544073f85f048497d26bc39fa
Chris Campbell <cacampbell@ucdavis.edu>
2016-05-05 14:20:31 -0700

    Removed module installation since this breaks the shiny.io deployment.

--- Alla-Dylan-Chris-Shiny_App/server.R
+++ Alla-Dylan-Chris-Shiny_App/server.R
@@ -2,9 +2,9 @@
 
 # Install Required Packages
 # http://stackoverflow.com/questions/4090169/elegant-way-to-check-for-missing-packages-and-install-them
-list.of.packages <- c("ggplot2")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+# list.of.packages <- c("ggplot2")
+# new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+# if(length(new.packages)) install.packages(new.packages)
 
 # Load required Libraries
 library(shiny)
