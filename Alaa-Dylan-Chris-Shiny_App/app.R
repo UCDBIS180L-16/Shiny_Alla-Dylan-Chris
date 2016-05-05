898fd506d6d7b652cd5e5b26f6eacd6bacaa7e81
Chris Campbell <cacampbell@ucdavis.edu>
2016-05-05 14:37:21 -0700

    Minor Change

--- Alaa-Dylan-Chris-Shiny_App/app.R
+++ Alaa-Dylan-Chris-Shiny_App/app.R
@@ -11,3 +11,4 @@ shinyApp(ui = ui, server = server)
 # runGitHub(repo="UCDBIS180L-16/Shiny_Alla-Dylan-Chris", username="cacampbell", subdir="Alaa-Dylan-Chris-Shiny_App")
 # rsconnect::deployApp('/Users/chris/Desktop/Classes/Spring2016/BIS180L/Shiny_Alla-Dylan-Chris/Alaa-Dylan-Chris-Shiny_App')
 # https://cacampbell.shinyapps.io/Alaa-Dylan-Chris-Shiny_App/
+# Alaa, Dylan -- please add your deployment code as a comment here if you want
