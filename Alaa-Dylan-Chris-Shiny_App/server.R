939b1636ef910a2203bdc4ff29e893362d4b4149
amhussein <amhussein@ucdavis.edu>
2016-05-10 14:53:50 -0700

    added options select different plots - Needs to be debugged

--- Alaa-Dylan-Chris-Shiny_App/server.R
+++ Alaa-Dylan-Chris-Shiny_App/server.R
@@ -27,35 +27,36 @@ shinyServer(function(input, output) {
     # draw the boxplot for the specified trait
     pl + geom_boxplot()
    })
-  })
   
-  #output$histogram <- renderPlot({
-   # pl2 <- ggplot(data=data.pheno.mds,aes_string(x="Amylose.content",
-    #                                             y=input$Amylose.content, 
-     #                                            fill="Region"))
+  output$histogram <- renderPlot({
+    
+    ShinyData <- read.csv("ShinyData.csv")
+    pl2 <- ggplot(data=ShinyData,aes_string(x="Region",
+                                                 y=input$trait, 
+                                                fill="Region"))
                   #Use aes_string below so that input$trait is interpreted
                   #correctly.  The other variables need to be quoted
     # set up the plot
-#    pl2 <- pl2 + geom_histogram(binwidth=3) #tell R that we want a histogram, with binwidth of 3
-#    pl2 <- pl2 + facet_wrap(facets= ~ Region, ncol=3) # a separate plot ("facet") for each region, arranged in 3 columns
-#    pl2 <- pl2 + ggtitle("Amylose Content") #add a title
-#    pl2 #display the plot
+   pl2 <- pl2 + geom_histogram(binwidth=3) #tell R that we want a histogram, with binwidth of 3
+   pl2 <- pl2 + facet_wrap(facets= ~ Region, ncol=3) # a separate plot ("facet") for each region, arranged in 3 columns
+   pl2 <- pl2 + ggtitle("Amylose Content") #add a title
+   pl2 #display the plot
   
-#  })
+ })
   
-#  output$violin <- renderPlot({
-    
+ output$violin <- renderPlot({
+   ShinyData <- read.csv("ShinyData.csv")
     # set up the plot
-#    pl3 <- qplot(data=data.pheno.mds,
+   pl3 <- ggplot(data=ShinyData,
                 #Use aes_string below so that input$trait is interpreted
                 #correctly.  The other variables need to be quoted
-#                aes_string(x="Amylose.content",
-#                           y=input$Amylose.content,
-#                           fill="Region"
-#                )
-#    )
+               aes_string(x="Region",
+                          y=input$trait,
+                          fill="Region"
+                )
+   )
     # draw the boxplot for the specified trait
-#    pl3 + geom_violin()
-#  })
-  
+   pl3 + geom_violin()
+  })
+})
 
