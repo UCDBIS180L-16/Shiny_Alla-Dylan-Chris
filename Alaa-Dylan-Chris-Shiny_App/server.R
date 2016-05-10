9bdc1aa50d6924e2951ad6ac2b609c900d38a329
Chris Campbell <cacampbell@ucdavis.edu>
2016-05-10 14:04:20 -0700

    Data import changed to separate function

--- Alaa-Dylan-Chris-Shiny_App/server.R
+++ Alaa-Dylan-Chris-Shiny_App/server.R
@@ -13,56 +13,45 @@ load_data <- function() {
         load("RiceSNPData/ps4.2500.RData")
     }, error=function(e) {
         print("Generating PSMix data and saving to .RData...")
-        ps4 <- PSMix(K=4, data.geno.2500, eps=1e-05)
+        ps4 <- PSMix(K = 4, data.geno.2500, eps = 1e-05)
         save(ps4, file="RiceSNPData/ps4.2500.RData")
     })
+
+    data.geno.2500 <- data.geno[, sample(1:dim(data.geno)[2], 2500)]
+    geno.numeric <- data.matrix(data.geno.2500)
+    genDist <- as.matrix(dist(geno.numeric))
+    geno.mds <- as.data.frame(cmdscale(genDist))
+    data.all <- merge(data.pheno, geno.mds, by='row.names', all=TRUE)
 }
 
 shinyServer(function(input, output) {
+    load_data()
 
-  output$boxplot <- renderPlot({
+    output$histogram <- renderPlot({
+        pl2 <- ggplot(data=data.pheno.mds,aes_string(x = "Region",
+                                                     y = input$trait,
+                                                     fill = "Region"))
+        #Use aes_string below so that input$trait is interpreted
+        #correctly.  The other variables need to be quoted
+        # set up the plot
+        pl2 <- pl2 + geom_histogram(binwidth=3) #tell R that we want a histogram, with binwidth of 3
+        pl2 <- pl2 + facet_wrap(facets = ~ Region, ncol = 3) # a separate plot ("facet") for each region, arranged in 3 columns
+        pl2 <- pl2 + ggtitle("Amylose Content") #add a title
+        pl2 #display the plot
+    })
 
-    # set up the plot
-    pl <- qplot(data=data.pheno.mds,
-                 #Use aes_string below so that input$trait is interpreted
-                 #correctly.  The other variables need to be quoted
-                 aes_string(x="Amylose.content",
-                            y=input$Amylose.content,
-                            fill="Region"
-                 )
-    )
-    # draw the boxplot for the specified trait
-    pl + geom_boxplot()
-   })
-  })
+    output$violin <- renderPlot({
 
-  #output$histogram <- renderPlot({
-   # pl2 <- ggplot(data=data.pheno.mds,aes_string(x="Amylose.content",
-    #                                             y=input$Amylose.content,
-     #                                            fill="Region"))
-                  #Use aes_string below so that input$trait is interpreted
-                  #correctly.  The other variables need to be quoted
-    # set up the plot
-#    pl2 <- pl2 + geom_histogram(binwidth=3) #tell R that we want a histogram, with binwidth of 3
-#    pl2 <- pl2 + facet_wrap(facets= ~ Region, ncol=3) # a separate plot ("facet") for each region, arranged in 3 columns
-#    pl2 <- pl2 + ggtitle("Amylose Content") #add a title
-#    pl2 #display the plot
-
-#  })
-
-#  output$violin <- renderPlot({
-
-    # set up the plot
-#    pl3 <- qplot(data=data.pheno.mds,
-                #Use aes_string below so that input$trait is interpreted
-                #correctly.  The other variables need to be quoted
-#                aes_string(x="Amylose.content",
-#                           y=input$Amylose.content,
-#                           fill="Region"
-#                )
-#    )
-    # draw the boxplot for the specified trait
-#    pl3 + geom_violin()
-#  })
-
-
+        #set up the plot
+        pl3 <- qplot(data=data.pheno.mds,
+                     #Use aes_string below so that input$trait is interpreted
+                     #correctly.  The other variables need to be quoted
+                     aes_string(x="Amylose.content",
+                                y=input$Amylose.content,
+                                fill="Region"
+                     )
+        )
+        #draw the boxplot for the specified trait
+        pl3 + geom_violin()
+    })
+})
--- Alaa-Dylan-Chris-Shiny_App/ui.R
+++ Alaa-Dylan-Chris-Shiny_App/ui.R
@@ -2,24 +2,24 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage( #create the overall page
-  
+
   # Application title
   titlePanel("RICE SNP DATA"),
-  
+
   # Some helpful information
   helpText("This application creates a different plots  to show difference between",
            "rice plants across different regions.  Please use the radio box below to choose a trait",
            "for plotting"),
-  
+
   # Sidebar with a radio box to input which trait will be plotted
   sidebarLayout(
     sidebarPanel(
-      radioButtons("Trait", #the input variable that the value will go into
+      radioButtons("trait", #the input variable that the value will go into
                    "Choose a trait to display:",
                    c("Amylose.content",
                      "Alu.Tol",
                      "Protein.content"))
-   #   radioButtons("Plot type", 
+   #   radioButtons("Plot type",
     #               "choose the desired plot:",
      #              c("histogram",
       #               "boxplot",
